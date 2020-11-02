f3f8b829ecbcb1234a88f7bd0a554e957e8bf4f8
Marcelo <marcelojunin2010@hotmail.com>
2020-11-02 20:19:55 -0300

    building header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Header = props => (
-    <View>
-        <Text>Text</Text>
+    <View style={style.container}>
+        <Text style={style.title}>Like</Text>
     </View>
 );
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: '#6ca2f7',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 20,
+        color: '#fff'
+    }
+});
 
 export default Header;

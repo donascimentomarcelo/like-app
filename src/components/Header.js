5987357703f6a68cfb584b3a811c4c4778009830
Marcelo <marcelojunin2010@hotmail.com>
2020-11-02 12:02:39 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = props => (
+    <View>
+        <Text>Text</Text>
+    </View>
+);
+
+export default Header;

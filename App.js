2c090b54786b879f3ce7d695091ff53cdde25c25
Marcelo <marcelojunin2010@hotmail.com>
2020-11-03 07:49:05 -0300

    changing app component to class component

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import Header from './src/components/Header';
+import * as CONST from './src/helpers/Constants';
 
-export default function App() {
-  return (
-    <View>
-      <Header/>
-    </View>
-  );
+export default class App extends React.Component {
+
+  render () {
+    return (
+      <View>
+        <Header 
+          title={CONST.TITLE}/>
+      </View>
+    )
+  }
 }

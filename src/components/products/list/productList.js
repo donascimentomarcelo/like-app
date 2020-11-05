96b85d319d947cf996b4b237c7ac125b9aa7a4d3
Marcelo <marcelojunin2010@hotmail.com>
2020-11-05 07:31:05 -0300

    showing products list

--- src/components/products/list/productList.js
+++ src/components/products/list/productList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 
 const ProductList = props => {
@@ -7,14 +7,36 @@ const ProductList = props => {
     const { products } = props || [];
     const elements =  products.map(product => {
         const { name, id } = product;
-        return <Text key={ id }>{ name }</Text>;
+        return (
+            <View key={ id } style={styles.line}>
+                <Text style={styles.lineText}>{ name }</Text>
+            </View>
+        );
     });
 
     return (
-        <View>
+        <View style={styles.container}>
             { elements }
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#e2f9ff'
+    },
+    line: {
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: '#bbb',
+
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    lineText: {
+        fontSize: 20,
+        paddingLeft: 15
+    }
+});
 
 export default ProductList;

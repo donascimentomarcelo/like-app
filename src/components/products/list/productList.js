b7917880901bb3f7534bbdd579b3f933a63a5bb9
Marcelo <marcelojunin2010@hotmail.com>
2020-11-07 14:19:59 -0300

    styling icon and product list

--- src/components/products/list/productList.js
+++ src/components/products/list/productList.js
@@ -6,18 +6,24 @@ const ProductList = props => {
 
     const { products, navigationDetail } = props || [];
     const elements =  products.map(product => {
-        const { id, name, price } = product;
+        const { id, name, price, description, oldPrice } = product;
         return (
             <TouchableOpacity 
                 key={ id } 
                 onPress={() => navigationDetail({ product })}>
-
+                
+                <Text style={styles.title}>{ `${capitalizeFristLetter(name)}` }</Text>
+                
                 <View style={styles.line}>
                     <Image 
                         style={styles.avatar}
                         source={{ uri: 'https://randomuser.me/api/portraits/thumb/women/66.jpg' }}/>
-                    <Text style={styles.lineText}>{ `${capitalizeFristLetter(name)} ${price}` }</Text>
+                    <Text style={styles.description}>{ description }</Text>
                 </View>
+                <View style={styles.line}>
+                    <Text style={styles.lineText}>{ verifyPrice(price, oldPrice) }</Text>
+                </View>
+
             </TouchableOpacity>
         );
     });
@@ -29,6 +35,17 @@ const ProductList = props => {
     );
 };
 
+const verifyPrice = (price, oldPrice) => {
+    return price && oldPrice ?
+     (<View style={styles.line}>
+        <Text style={styles.oldPrice}> De R$ { oldPrice } </Text> 
+        <Text style={styles.currentPrice}> 
+                Por R$ { price }
+        </Text>
+     </View>) :
+     (<Text style={styles.lineText}>R$ {price} </Text>);
+}
+
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#f0efef'
@@ -38,14 +55,35 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'row'
     },
+    title : {
+        fontWeight: 'bold',
+        paddingLeft: 20
+    },
     lineText: {
         fontSize: 15,
+        paddingLeft: 15,
+        flex: 7
+    },
+    currentPrice: {
+        fontSize: 15,
+        color: 'green',
+        marginLeft: 10
+    },
+    oldPrice: {
+        fontSize: 10,
+        color: 'red',
+        textDecorationLine: 'line-through',
+    },
+    description: {
+        color: 'gray',
+        fontSize: 13,
         paddingLeft: 15,
         flex: 7
     },
     avatar: {
         aspectRatio: 1,
         width: 50,
+        height: 50,
         flex: 1,
 
         marginLeft: 15,

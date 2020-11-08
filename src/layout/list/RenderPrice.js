628e385807653d0c6029cf67a958c0b06eade52c
Marcelo <marcelojunin2010@hotmail.com>
2020-11-08 20:26:48 -0300

    align price to right

--- src/layout/list/RenderPrice.js
+++ src/layout/list/RenderPrice.js
@@ -3,25 +3,29 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const RenderPrice = ({price, oldPrice}) => {
     return price && oldPrice ?
-        (<View style={styles.line}>
-        <Text style={styles.oldPrice}> De R$ { oldPrice } </Text> 
-        <Text style={styles.currentPrice}> 
-                Por R$ { price }
-        </Text>
-        </View>) :
-        (<Text style={styles.lineText}>R$ {price} </Text>);
+        (
+            <View style={styles.alignPrice}>
+                <Text style={styles.oldPrice}> De R$ { oldPrice } </Text> 
+                <Text style={styles.currentPrice}> 
+                        Por R$ { price }
+                </Text>
+            </View> 
+        ) :
+        (
+            <View style={styles.alignPrice}>
+                <Text style={styles.alonePrice}>
+                    R$ {price} 
+                </Text>
+            </View>
+        );
 }
 
 const styles = StyleSheet.create({
-    line: {
-        height: 60,
-        alignItems: 'center',
-        flexDirection: 'row'
-    },
-    lineText: {
-        fontSize: 15,
-        paddingLeft: 15,
-        flex: 7
+
+    alignPrice: {
+        alignSelf: 'flex-end',
+        flexDirection: 'row',
+        marginRight: 20
     },
     currentPrice: {
         fontSize: 15,

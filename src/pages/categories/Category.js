5474eb791c7b86a9eccff6ed6a24b22155e64ef6
Marcelo <marcelojunin2010@hotmail.com>
2021-01-20 23:03:25 -0300

    refactoring category load data

--- src/pages/categories/Category.js
+++ src/pages/categories/Category.js
@@ -2,55 +2,82 @@ import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 import CategoryList from './list/CategoryList';
+import ErrorComponent from '../../layout/error/ErrorComponent';
+import LoadingComponent from '../../layout/loading/LoadingComponent';
 
 import * as Env from './../../helpers/Env';
+import * as CONST from './../../helpers/Constants';
 import Axios from 'axios';
 
 export default class Category extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            categories: [],
-            loading: false,
-            hasError: false
-        };
-    }
+    this.state = {
+      categories: [],
+      loading: false,
+      hasError: false
+    };
+  }
 
-    componentDidMount() {
-        this.setState({hasError: false, loading: true})
-        Axios
-          .get(`${Env.LOCALHOST}${Env.CATEGORIES}`)
-            .then(resp => {
-              const { data } = resp;
-              this.setState({
-                categories: data,
-                loading: false
-              })
-            })
-            .catch(error => {
-              this.setState({
-                hasError: true, 
-                loading: false
-              });
-            });
-      }
+  componentDidMount() {
+    this.setState({ hasError: false, loading: true })
+    Axios
+      .get(`${Env.LOCALHOST}${Env.CATEGORIES}`)
+      .then(resp => {
+        const { data } = resp;
+        this.setState({
+          categories: data,
+          loading: false
+        })
+      })
+      .catch(error => {
+        this.setState({
+          hasError: true,
+          loading: false
+        });
+      });
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <CategoryList
-                    categories={this.state.categories}
-                    navigationDetail={(category) => this.props.navigation.navigate('ProductByCategory', category)}/>
-            </View>
-        )
-    }
+
+  renderError() {
+    return <ErrorComponent
+      hasError={this.state.hasError}
+      message={CONST.SOMETHING_IS_WROG} />
+  }
+
+  loading() {
+    return <LoadingComponent
+      loading={this.state.loading}
+      hasError={this.state.hasError}
+      size={CONST.LARGE}
+      color={CONST.PRIMARY} />
+  }
+
+  listCategories() {
+    return <CategoryList
+      categories={this.state.categories}
+      navigationDetail={(category) => this.props.navigation.navigate('ProductByCategory', category)} />
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {
+          !this.state.loading && !this.state.hasError ?
+            this.listCategories() :
+            this.loading()
+        }
+        { this.renderError()}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
 })
